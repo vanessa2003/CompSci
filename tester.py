67cbf1630664be7d3e127281d3bf359e68822577
vanessa2003 <68717311+vanessa2003@users.noreply.github.com>
2020-07-27 12:12:20 +0100

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,4 +1,4 @@
 Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> print("this will be the virus simulator")
+>>> print("this will be the tester")
 print("second check")
