a77c52709f4eb78fd213ed1e2e5e4093a537695b
bparsia <bijan.parsia@manchester.ac.uk>
2020-08-05 12:17:08 +0100

    Update TrivialExample.py

--- TrivialExample.py
+++ TrivialExample.py
@@ -27,6 +27,15 @@ class State(IntEnum):
     sick = 2
     recovered = 3
 
+# BP: Consider using a dictionary instead?
+# [1,2,3][0] => 1
+# {1:'one', 2:'two', 3:'three'}[1] => 'one'
+# {2:'two', 1:'one', 3:'three'}
+# {'one':1, 'two':2, 'three':3}['one'] => 1
+# population_stats = {'tsick_pop':0}
+# population_stats['tsick_pop'] += 1
+
+
 class population():
     tsick_pop = 0
     newly_infected = 0
@@ -44,43 +53,69 @@ class SimpleAgent(Agent):
         self.state = State.susceptible
         self.infection_duration = 0
 
+
     def step_prologue(self, model):
         print("This is the prologue, so I will say my name: {0}".format(
             self.name))
         
-
+# Transmission model
+# Propability of being infected is proportional to total number of infectous people.
+# "susceptible" perspective
+#    if I am suceptible, evaluate my chance of being infected and then check whether I am infected
+# "infectious" perspective
+#    if I am infectous, evaluate my chance of *infecting* every suceptible
     def step_main(self, model):
         a = (tuple(model.schedule.agents))
+        # BP: a = model.schedule.agents
         for person in a:
             infected = np.random.choice([0,1], p=[0.9,0.1])
             print(infected)
             if infected == 1:
                 self.state = State.infected
                 population.newly_infected +=1
-        for someone in a:
-            if self.state is State.infected and someone.state is State.susceptible:
-                if random.random() >= 0.5:
-                    #print(random.random())
-                    someone.state = State.infected
-                    #someone.infection_duration = self.model.schedule.time
+        if self.state is State.infected:
+            for someone in a:
+                if someone.state is State.susceptible:
+                    if random.random() >= 0.5:
+                        #print(random.random())
+                        someone.state = State.infected
+                        #someone.infection_duration = self.model.schedule.time
+            # update length of infection
           
 
 
     def step_epilogue(self, model):
+        # Update state
+        # infected => infectious (sick)
+        # infectious => recovered
+        
+        # Update statistics
+        # 
+        # BP: Loop is wrong
         for i in range (model.epochs):
             if self.state == State.infected:
+                newly_infect += 1
+                tsick_pop += 1
                 self.state == State.sick
-                population.tsick_pop = population.newly_infected
+                population.tsick_pop += population.newly_infected
                 print("The total number of infected people in this epoch is,",population.tsick_pop)
-
+            if selfstate == State.sick and length_of_infect == recoverytime:
+                #update state
+                population.tsick_pop -= 1
 
              #t = model.schedule.time-self.infection_duration
              #print("there are {0} agents infected".format(model.properties["infected"]))
 
+# Stats at end of epoch:
+#   How many newly infected
+#   Current total sick == number currently infectious
+#   Cumulative total == current infectious + recovered == recovered
+                
 class SimpleHelper(Helper):
 
     def step_prologue(self, model):
         print("Helper prologue")
+        # zero out the newly_infected
         
     
 
