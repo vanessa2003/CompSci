c4d91090f5b2d13398fc0430923878c55ddb48bc
vanessa neboh <vanessaneboh15@gmail.com>
2020-08-05 11:06:50 +0100

    new changes added

--- TrivialExample.py
+++ TrivialExample.py
@@ -1,5 +1,6 @@
 from panaxea.core.Model import Model
 from panaxea.core.Steppables import Agent, Helper
+from enum import IntEnum
 import time
 import numpy as np
 """
@@ -20,17 +21,24 @@ import random
 
 
 
-class State():
+class State(IntEnum):
     susceptible = 0
     infected = 1
-    recovered = 0
+    sick = 2
+    recovered = 3
 
+class population():
+    tsick_pop = 0
+    newly_infected = 0
+    susceptible_pop = 0
+    recovered_pop = 0
+    
 
 class SimpleAgent(Agent):
     
 
     def __init__(self, name):
-        super(SimpleAgent, self,).__init__()
+        super(SimpleAgent, self).__init__()
         self.name = name
         self.age = random.normalvariate(20,100)
         self.state = State.susceptible
@@ -39,38 +47,46 @@ class SimpleAgent(Agent):
     def step_prologue(self, model):
         print("This is the prologue, so I will say my name: {0}".format(
             self.name))
-         
+        
 
-        """
     def step_main(self, model):
-        people = model.schedule.agents 
-        if random.random() >= 0.5:
-            for other in people:
-                if self.state is State.infected and other.state is State.susceptible:
-                    other.state = State.infected
-                    other.infection_duration = self.model.schedule.time
-                    other.recovery_duration = model.the_recovery_time()
-                    """
-
+        a = (tuple(model.schedule.agents))
+        for person in a:
+            infected = np.random.choice([0,1], p=[0.9,0.1])
+            print(infected)
+            if infected == 1:
+                self.state = State.infected
+                population.newly_infected +=1
+        for someone in a:
+            if self.state is State.infected and someone.state is State.susceptible:
+                if random.random() >= 0.5:
+                    #print(random.random())
+                    someone.state = State.infected
+                    #someone.infection_duration = self.model.schedule.time
           
-    def step_epilogue(self, model):
-        print("This is the epilogue, and {0} says the counter is set to"
-              " {1}".format(self.name, model.properties["counter"]))
-        if self.state == State.infected:
-             #t = model.schedule.time-self.infection_duration
-             print("there are {0} agents infected".format(model.properties["infected"]))
 
+
+    def step_epilogue(self, model):
+        for i in range (model.epochs):
+            if self.state == State.infected:
+                self.state == State.sick
+                population.tsick_pop = population.newly_infected
+                print("The total number of infected people in this epoch is,",population.tsick_pop)
+
+
+             #t = model.schedule.time-self.infection_duration
+             #print("there are {0} agents infected".format(model.properties["infected"]))
 
 class SimpleHelper(Helper):
 
     def step_prologue(self, model):
-        model.properties["counter"] += 1
-        for i in model.schedule.agents:
-            infected = np.random.choice([0,1], p=[0.9,0.1])
-            if infected == 1:
-                self.state = State.infected
-                model.properties["infected"] += 1
-       
+        print("Helper prologue")
+        
+    
+
+    def step_epilogue(self,model):
+        print("Helper epilogue")
+        #print(State.infected)
 """
 class simplemodel(model):
 
@@ -88,8 +104,8 @@ model.properties["counter"] = 0
 model.properties["infected"] = 0
 model.properties["susceptible"] = 0
 model.properties["recovered"] = 0
-model.properties["population"] = model.schedule.agents
-model.schedule.agents_to_schedule.add(SimpleAgent("Adam"))
+#model.properties["population"] = model.schedule.agents
+#model.schedule.agents_to_schedule.add(SimpleAgent("Adam"))
 model.schedule.agents_to_schedule.add(SimpleAgent("Beth"))
 model.schedule.agents_to_schedule.add(SimpleAgent("james"))
 model.schedule.agents_to_schedule.add(SimpleAgent("sarah"))
